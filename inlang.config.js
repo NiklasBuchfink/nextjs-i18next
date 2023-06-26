5f550e5ff2638454f4b3455029434dbf6145b9e7
NiklasBuchfink <niklas.buchfink@web.de>
2023-06-26 16:16:30 +0200

    update plugin

--- inlang.config.js
+++ inlang.config.js
@@ -4,7 +4,7 @@
 export async function defineConfig(env) {
   // const { default: pluginJson } = await env.$import("./i18next.js");
   const { default: pluginJson } = await env.$import(
-    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2.4.0/dist/index"
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2.4.0/dist/index.js"
   );
 
   const { default: standardLintRules } = await env.$import(
