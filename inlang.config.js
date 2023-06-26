1c1a1a0134651aa13e3c2ae16db4cd9fbfd5077a
NiklasBuchfink <niklas.buchfink@web.de>
2023-06-26 17:19:16 +0200

    update plugin

--- inlang.config.js
+++ inlang.config.js
@@ -2,10 +2,10 @@
  * @type { import("@inlang/core/config").DefineConfig }
  */
 export async function defineConfig(env) {
-  // const { default: pluginJson } = await env.$import("./i18next.js");
-  const { default: pluginJson } = await env.$import(
-    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2.4.0/dist/index.js"
-  );
+  const { default: pluginJson } = await env.$import("./i18next.js");
+  // const { default: pluginJson } = await env.$import(
+  //   "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2.4.0/dist/index.js"
+  // );
 
   const { default: standardLintRules } = await env.$import(
     "https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js"
