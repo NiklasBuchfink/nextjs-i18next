eb4eaa7028666198c65c5a82db2d86bb4a4b70e9
NiklasBuchfink <niklas.buchfink@web.de>
2023-07-04 11:30:58 +0200

    update config

--- inlang.config.js
+++ inlang.config.js
@@ -2,7 +2,9 @@
  * @type { import("@inlang/core/config").DefineConfig }
  */
 export async function defineConfig(env) {
-  const { default: i18nextPlugin } = await env.$import("./plugin.js");
+  const { default: i18nextPlugin } = await env.$import(
+    "https://cdn.jsdelivr.net/gh/inlang/plugin-i18next@latest/dist/index.js"
+  );
 
   const { default: standardLintRules } = await env.$import(
     "https://cdn.jsdelivr.net/npm/@inlang/plugin-standard-lint-rules@3/dist/index.js"
